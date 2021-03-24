1a1d3bb3c2668082a8c5d20787b25ca19a7c8026
zzcheah <zzcheah@users.noreply.github.com>
2021-03-24 22:13:29 +0800

    -> private route -> react context instead of redux -> cleaner code for app.js -> loading component (blur and disable touch)

--- src/App.js
+++ src/App.js
@@ -1,42 +1,99 @@
-import {
-  Header,
-  HeaderMenuButton,
-  HeaderName,
-  SkipToContent,
-  HeaderContainer,
-} from "carbon-components-react";
 import StoryContent from "./components/layout/StoryContent";
 import RequestForm from "./components/request/RequestForm";
-import ActionBar from "./components/shell/ActionBar";
-import LeftPanel from "./components/shell/LeftPanel";
-import MyRequests from "./pages/MyRequests";
 
-function App() {
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+
+import MyRequests from "./pages/MyRequests";
+import AppHeader from "./components/layout/AppHeader";
+import SignupPage from "./components/auth/SignupPage";
+import LoginPage from "./components/auth/LoginPage";
+import { createElement, useState } from "react";
+import { Loading } from "carbon-components-react";
+import AppContext from "./context/AppContext";
+
+function LoadingBlur({ loading, children }) {
+  const style = {
+    loading: {
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      textAlign: "center",
+    },
+    blur: {
+      opacity: loading ? "0.7" : "1",
+      pointerEvent: loading ? "none" : "",
+    },
+  };
   return (
     <div>
-      <HeaderContainer
-        render={({ isSideNavExpanded, onClickSideNavExpand }) => (
-          <>
-            <Header aria-label="IBM Platform Name">
-              <SkipToContent />
-              <HeaderMenuButton
-                aria-label="Open menu"
-                onClick={onClickSideNavExpand}
-                isActive={isSideNavExpanded}
-              />
-              <HeaderName href="#" prefix="">
-                GPU Yard
-              </HeaderName>
-              <ActionBar />
-              <LeftPanel show={isSideNavExpanded} />
-            </Header>
-          </>
-        )}
-      />
-      <StoryContent>
-        {/* <MyRequests /> */}
-        <RequestForm />
-      </StoryContent>
+      {loading ? (
+        <div style={style.loading}>
+          <Loading />
+        </div>
+      ) : null}
+      <div style={style.blur}>{children}</div>
+    </div>
+  );
+}
+
+function PrivateRoute({ component, ...rest }) {
+  const { user } = rest;
+  return (
+    <Route
+      {...rest}
+      render={({ location }) =>
+        user ? (
+          createElement(component)
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/login",
+              state: { from: location },
+            }}
+          />
+        )
+      }
+    />
+  );
+}
+
+function App() {
+  // get auth loading and user
+  // put into loading when auth loading
+
+  const [loading, setLoading] = useState(false);
+  const [user, setUser] = useState("ali");
+  const appValue = {
+    app: {
+      loading,
+      setLoading,
+    },
+    auth: {
+      user,
+      setUser,
+    },
+  };
+
+  return (
+    <div>
+      <BrowserRouter>
+        <LoadingBlur loading={loading}>
+          <AppHeader auth={user} />
+          <AppContext.Provider value={appValue}>
+            <StoryContent>
+              <Switch>
+                <Route exact path="/login" component={LoginPage} />
+                <Route exact path="/signup" component={SignupPage} />
+                <PrivateRoute exact path="/" component={MyRequests} />
+                <PrivateRoute path="/requestForm" component={RequestForm} />
+              </Switch>
+            </StoryContent>
+          </AppContext.Provider>
+        </LoadingBlur>
+      </BrowserRouter>
+
+      {/* <MyRequests /> */}
+      {/* <RequestForm /> */}
     </div>
   );
 }
