021d59c96bf98a6ee73c09b4eecd48a998a7cfe7
zzcheah <zzcheah@users.noreply.github.com>
2021-03-29 21:01:39 +0800

    -> added new dependecies (axios, jwt-decode) -> added functionality to restore user session from local storage -> verify if the jwt is expired, remove if expired -> created service to handle auth related actions -> replaced mock api fetching with real backend endpoint -> replaced fetch with axios ->

--- src/contexts/AppContext.js
+++ src/contexts/AppContext.js
@@ -1,13 +1,34 @@
 import { createContext, useEffect, useReducer } from "react";
 import { appReducer } from "../reducers/AppReducer";
+import axios from "axios";
+import jwt_decode from "jwt-decode";
 
 export const AppContext = createContext();
 
 const AppContextProvider = (props) => {
+  var auth = null;
+
+  if (localStorage.authJwt) {
+    const decoded = jwt_decode(localStorage.authJwt);
+    var current_time = new Date().getTime() / 1000;
+    const profileString = localStorage.getItem("profile");
+    if (profileString) {
+      auth = current_time > decoded.exp ? null : JSON.parse(profileString);
+    }
+  }
+
+  if (auth) {
+    axios.defaults.headers.common[
+      "Authorization"
+    ] = `Bearer ${localStorage.authJwt}`;
+  } else {
+    delete axios.defaults.headers.common["Authorization"];
+  }
+
   const [appState, dispatch] = useReducer(appReducer, {}, () => {
     return {
       loading: false,
-      auth: false,
+      auth,
       toast: null,
     };
   });
